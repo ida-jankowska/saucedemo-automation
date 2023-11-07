dcd169a49e204751b70529d5df242a58db78a3c1
Ida Jankowska <ida.jankowska@onet.pl>
2023-11-07 17:49:38 +0100

    Add methods to add/remove items from shopping cart

--- src/pages/homePage.ts
+++ src/pages/homePage.ts
@@ -9,6 +9,11 @@ export class HomePage {
     this.page = page;
   }
 
+  buttonsText = {
+    addToCartBtn: 'Add To Cart',
+    removeBtn: 'Remove'
+}
+
   async checkIfSecondaryHeaderIsVisible() {
     const secondaryHeader = this.page.locator(locators.secondaryHeader);
     return await secondaryHeader.isVisible();
@@ -17,5 +22,33 @@ export class HomePage {
   async userLogout() {
     await this.page.locator(locators.openMenuButton).click();
     await this.page.locator(locators.logoutButton).click();
+  }
+
+  async countInventoryItems() {
+    const inventoryItems = await this.page.$$(locators.inventoryItem);
+    expect(inventoryItems).not.toBeNull();
+    return inventoryItems.length;
+  }
+
+  async checkIfShoppingCartIsEmpty() {
+    const itemsInCartCounter = this.page.locator(locators.shoppingCartBadge);
+    await expect(itemsInCartCounter).toHaveCount(0);
+  }
+
+  async addItemToCart(itemName : string) {
+    const itemToPurchase = this.page.locator(locators.inventoryItem, { hasText: itemName});
+    const addToCartButton = itemToPurchase.locator(locators.addToCartButton, { hasText: this.buttonsText.addToCartBtn});
+    await addToCartButton.click()
+  }
+
+  async removeItemFromCart(itemName : string) {
+    const itemToRemove = this.page.locator(locators.inventoryItem, { hasText: itemName});
+    const removeFromCartButton = itemToRemove.locator(locators.removeFromCartButton, { hasText: this.buttonsText.removeBtn});
+    await removeFromCartButton.click()
+  }
+
+  async getNumberOfItemsAddedToCart() {
+    const itemsInCartCounter = this.page.locator(locators.shoppingCartBadge);
+    return await itemsInCartCounter?.textContent();
   }
 }
