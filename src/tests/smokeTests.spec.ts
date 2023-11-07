e5ee305ae3828ea5b8ccfb63d1179c8a804d24ce
Ida Jankowska <ida.jankowska@onet.pl>
2023-11-07 17:50:20 +0100

    Add new test 'User can add products to their shopping cart and remove them'

--- src/tests/smokeTests.spec.ts
+++ src/tests/smokeTests.spec.ts
@@ -3,8 +3,16 @@ import { SAUCE_DEMO_ADDRESS } from '../config/environmentParameters';
 import { LoginPage } from '../pages/loginPage';
 import { HomePage } from '../pages/homePage';
 
+test.afterEach(async ({ page }, testInfo) => {
+  console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+
+  if (testInfo.status !== testInfo.expectedStatus)
+    console.log(`Did not run as expected, ended up at ${page.url()}`);
+});
+
 test.describe('Sauce Demo smoke tests', () => {
   const invalidCredentialsErrorMsg : string = 'Epic sadface: Username and password do not match any user in this service';
+  const storeInventoryItems : number = 6;
 
   const userData = {
     username: 'standard_user',
@@ -44,12 +52,29 @@ test.describe('Sauce Demo smoke tests', () => {
     expect(errorMessageText).toBe(invalidCredentialsErrorMsg);
   });
 
-// todo
+  test('User can add products to their shopping cart and remove them', async ({ page }) => {
+    const sauceDemoLoginPage = new LoginPage(page);
+    await sauceDemoLoginPage.goto();
+    await sauceDemoLoginPage.userLogin(userData.username, userData.password);
 
-  // test('User can add products to their shopping cart', async ({ page }) => {
+    const sauceDemoHomePage = new HomePage(page);
+    const actualInventoryItems : number = await sauceDemoHomePage.countInventoryItems();
+    expect (actualInventoryItems).toBe(storeInventoryItems);
 
+    await sauceDemoHomePage.checkIfShoppingCartIsEmpty();
 
-  // });
+    const itemsToPurchase : string[] = ['Sauce Labs Backpack', 'Sauce Labs Fleece Jacket', 'Sauce Labs Onesie'];
+    for (const item of itemsToPurchase) {
+      await sauceDemoHomePage.addItemToCart(item);
+  }
+
+  let itemsInShoppingCart = await sauceDemoHomePage.getNumberOfItemsAddedToCart();
+  expect (Number(itemsInShoppingCart)).toBe(itemsToPurchase.length);
+
+  await sauceDemoHomePage.removeItemFromCart(itemsToPurchase[0]);
+  itemsInShoppingCart = await sauceDemoHomePage.getNumberOfItemsAddedToCart();
+  expect (Number(itemsInShoppingCart)).toBe(itemsToPurchase.length-1);
+});
 
   // todo
   // test('User can place an order', async ({ page }) => {
